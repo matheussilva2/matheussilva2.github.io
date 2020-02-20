4d570a0dfef71d749e525d40d87c52072d91dfa3
Matheus <matheusnascimentoeoq@gmail.com>
2020-02-20 05:08:15 -0200

    Jogo The Guy

---
+++ projetos/the-guy/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1582182271,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ground-sheet0.png",
+		"images/character-sheet0.png",
+		"images/character-sheet1.png",
+		"images/invisiblewall-sheet0.png",
+		"images/playerbox-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/enemy-sheet1.png",
+		"images/edgemarker-sheet0.png",
+		"images/floatground-sheet0.png",
+		"images/sky-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/ground_bg-sheet0.png",
+		"images/crypt-sheet0.png",
+		"images/tumulos-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
